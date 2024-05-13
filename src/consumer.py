7ef44c2eaea9e6cd71e4c09a7c022d7d6a2b5023
Gill Mertens <gill.mertens@student.ehb.be>
2024-05-13 11:26:41 +0200

    Fix: remove redundant logs

--- src/consumer.py
+++ src/consumer.py
@@ -332,12 +332,6 @@ def initialize_logger(logger):
     logger.addHandler(c_handler)
     logger.addHandler(s_handler)
 
-    logger.debug('This is a debug message')
-    logger.info('This is an info message')
-
-    logger.warning('This is a warning')
-    logger.error('This is an error')
-
 
 if __name__ == '__main__':
     try:
