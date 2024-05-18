da23ecc1b1b921a26eb6aa5a7e590ec9e4ed58ce
ThomasKnoops <thomas@knoops.be>
2024-05-18 17:49:14 +0200

    removed port

--- src/publisher.py
+++ src/publisher.py
@@ -23,7 +23,7 @@ def main():
     #credentials = pika.PlainCredentials(secrets.RABBITMQ_USER, secrets.RABBITMQ_PASSWORD)
     credentials = pika.PlainCredentials('user', 'password')
     try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=secrets.HOST, port=secrets.PORT, credentials=credentials))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=secrets.HOST, credentials=credentials))
     except Exception as e:
         logger.error(f"Failed to connect to RabbitMQ: {e}")
         log(logger, "CONSUMER", f"Failed to connect to RabbitMQ: {e}", error='true')
