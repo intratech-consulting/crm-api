8668aea6c6f3cba112b5e3aa80600bdc950fdffa
ThomasKnoops <thomas@knoops.be>
2024-05-23 10:22:35 +0200

    tests: Publisher test user

--- tests/test_publisher_new.py
+++ tests/test_publisher_new.py
@@ -26,7 +26,6 @@ class text_colors:
     UNDERLINE = '\033[4m'
 
 class TestPublisher(unittest.TestCase):
-    warnings.filterwarnings("ignore")
     @classmethod
     def setUp(self):
         self.rabbitmq_message = None
@@ -191,6 +190,230 @@ class TestPublisher(unittest.TestCase):
                 raise
             else:
                 print(f"{text_colors.OKGREEN}##### Test 1 passed #####{text_colors.ENDC}")
+
+    def test_02_user_update(self):
+        # Mock the get updated objects API response
+        user_object = {
+            'ChangeEventHeader': {
+                'entityName': 'user__c',
+                'recordIds': ['a04Qy000000FLybIAG'],
+                'changeType': 'UPDATE',
+                'changeOrigin': 'com/salesforce/api/soap/60.0;client=SfdcInternalAPI/',
+                'transactionKey': '00003357-a4cd-8697-1ea3-1e779d24d4d9',
+                'sequenceNumber': 1,
+                'commitTimestamp': 1716381784000,
+                'commitNumber': 1716381784922292225,
+                'commitUser': '005Qy000003Cg1lIAC',
+                'nulledFields': [],
+                'diffFields': [],
+                'changedFields': ['0x1F8040']
+            },
+            'OwnerId': None,
+            'Name': None,
+            'RecordTypeId': None,
+            'CreatedDate': None,
+            'CreatedById': None,
+            'LastModifiedDate': 1716451799000,
+            'LastModifiedById': None,
+            'email__c': None,
+            'first_name__c': None,
+            'last_name__c': None,
+            'company_id__c': None,
+            'company_email__c': None,
+            'source__c': None,
+            'calendar_link__c': None,
+            'country__c': 'Nederland',
+            'state__c': 'BA',
+            'city__c': 'Amsterdam',
+            'zip__c': 1045.0,
+            'street__c': 'Australiëhavenweg',
+            'house_number__c': 21.0,
+            'telephone__c': None,
+            'birthday__c': None,
+            'invoice__c': None,
+            'user_role__c': None
+        }
+
+        # Mock the masteruuid API response
+        mock_masteruuid_response = MagicMock()
+        mock_masteruuid_response.status_code = 200
+        mock_masteruuid_response.json.return_value = {
+            "success": True,
+            "UUID": 'df59f548-538c-46f5-9f7f-66dacc3fcd82'
+        }
+
+        # The expected assertion message
+        expected_message = '''
+        <user>
+            <routing_key>user.crm</routing_key>
+            <crud_operation>update</crud_operation>
+            <id>df59f548-538c-46f5-9f7f-66dacc3fcd82</id>
+            <first_name />
+            <last_name />
+            <email />
+            <telephone />
+            <birthday />
+            <address>
+                <country>Nederland</country>
+                <state>BA</state>
+                <city>Amsterdam</city>
+                <zip>1045</zip>
+                <street>Australiëhavenweg</street>
+                <house_number>21</house_number>
+            </address>
+            <company_email />
+            <company_id />
+            <source />
+            <user_role />
+            <invoice />
+            <calendar_link />
+        </user>
+        '''
+
+        with (
+            patch('src.uuidapi.requests.post') as mock_post,
+            patch('src.publisher.log', return_value={'success': True, 'message': 'Log successfully added.'})
+        ):
+            channel: BlockingChannel = self.configure_rabbitMQ()
+            mock_post.return_value = mock_masteruuid_response
+
+            def run_publisher():
+                with patch('src.uuidapi.requests.post', mock_post), \
+                        patch('src.publisher.log', return_value={'success': True, 'message': 'Log successfully added.'}):
+                    publisher.handle_change_event(user_object)
+
+            # Consume the message from RabbitMQ in a separate thread
+            consume_thread = threading.Thread(target=self.start_consuming, args=(channel,))
+            consume_thread.daemon = True
+            consume_thread.start()
+
+            publisher_thread = threading.Thread(target=run_publisher)
+            publisher_thread.daemon = True
+            publisher_thread.start()
+
+            # Wait for the callback to be called
+            self.callback_event.wait(timeout=10)
+
+            # Assert that the message is the same as the expected message
+            try:
+                self.assertEqual(''.join((self.rabbitmq_message).split()), ''.join(expected_message.split()))
+            except AssertionError:
+                print(f"{text_colors.FAIL}##### Test 2 failed #####{text_colors.ENDC}")
+                raise
+            else:
+                print(f"{text_colors.OKGREEN}##### Test 2 passed #####{text_colors.ENDC}")
+
+    def test_03_user_delete(self):
+        # Mock the get updated objects API response
+        user_object = {
+            'ChangeEventHeader': {
+                'entityName': 'user__c',
+                'recordIds': ['a04Qy000000FLybIAG'],
+                'changeType': 'DELETE',
+                'changeOrigin': 'com/salesforce/api/soap/60.0;client=SfdcInternalAPI/',
+                'transactionKey': '00003357-a4cd-8697-1ea3-1e779d24d4d9',
+                'sequenceNumber': 1,
+                'commitTimestamp': 1716381784000,
+                'commitNumber': 1716381784922292225,
+                'commitUser': '005Qy000003Cg1lIAC',
+                'nulledFields': [],
+                'diffFields': [],
+                'changedFields': []
+            },
+            'OwnerId': None,
+            'Name': None,
+            'RecordTypeId': None,
+            'CreatedDate': None,
+            'CreatedById': None,
+            'LastModifiedDate': None,
+            'LastModifiedById': None,
+            'email__c': None,
+            'first_name__c': None,
+            'last_name__c': None,
+            'company_id__c': None,
+            'company_email__c': None,
+            'source__c': None,
+            'calendar_link__c': None,
+            'country__c': None,
+            'state__c': None,
+            'city__c': None,
+            'zip__c': None,
+            'street__c': None,
+            'house_number__c': None,
+            'telephone__c': None,
+            'birthday__c': None,
+            'invoice__c': None,
+            'user_role__c': None
+        }
+        
+        # Mock the masteruuid API response
+        mock_masteruuid_response = MagicMock()
+        mock_masteruuid_response.status_code = 200
+        mock_masteruuid_response.json.return_value = {
+            "success": True,
+            "UUID": 'df59f548-538c-46f5-9f7f-66dacc3fcd82'
+        }
+
+        # The expected assertion message
+        expected_message = '''
+        <user>
+            <routing_key>user.crm</routing_key>
+            <crud_operation>delete</crud_operation>
+            <id>df59f548-538c-46f5-9f7f-66dacc3fcd82</id>
+            <first_name />
+            <last_name />
+            <email />
+            <telephone />
+            <birthday />
+            <address>
+                <country />
+                <state />
+                <city />
+                <zip />
+                <street />
+                <house_number />
+            </address>
+            <company_email />
+            <company_id />
+            <source />
+            <user_role />
+            <invoice />
+            <calendar_link />
+        </user>
+        '''
+
+        with (
+            patch('src.uuidapi.requests.post') as mock_post,
+            patch('src.publisher.log', return_value={'success': True, 'message': 'Log successfully added.'})
+        ):
+            channel: BlockingChannel = self.configure_rabbitMQ()
+            mock_post.return_value = mock_masteruuid_response
+
+            def run_publisher():
+                with patch('src.uuidapi.requests.post', mock_post), \
+                        patch('src.publisher.log', return_value={'success': True, 'message': 'Log successfully added.'}):
+                    publisher.handle_change_event(user_object)
+
+            # Consume the message from RabbitMQ in a separate thread
+            consume_thread = threading.Thread(target=self.start_consuming, args=(channel,))
+            consume_thread.daemon = True
+            consume_thread.start()
+
+            publisher_thread = threading.Thread(target=run_publisher)
+            publisher_thread.daemon = True
+            publisher_thread.start()
+
+            # Wait for the callback to be called
+            self.callback_event.wait(timeout=10)
+
+            # Assert that the message is the same as the expected message
+            try:
+                self.assertEqual(''.join((self.rabbitmq_message).split()), ''.join(expected_message.split()))
+            except AssertionError:
+                print(f"{text_colors.FAIL}##### Test 3 failed #####{text_colors.ENDC}")
+                raise
+            else:
+                print(f"{text_colors.OKGREEN}##### Test 3 passed #####{text_colors.ENDC}")
 
 if __name__ == "__main__":
     unittest.main()
